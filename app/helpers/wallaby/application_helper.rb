947e35aa4f895e8f93843baf005f9ccdaa77039c
Tian Chen <me@tian.im>
2015-07-25 07:47:10 +1000

    override the origin render method

--- app/helpers/wallaby/application_helper.rb
+++ app/helpers/wallaby/application_helper.rb
@@ -8,24 +8,10 @@ module Wallaby
       ].compact.join ' '
     end
 
-    def render_partial partial_view_path, options = {}
-      ancestors = controller.class.ancestors.select do |constance|
-        constance <= ::ApplicationController
-      end
-
-      full_view_paths = ancestors.map do |klass|
-        "#{ klass.name.gsub(%r((Application)?Controller), '').underscore }/#{ partial_view_path }".gsub('//', '/').gsub(%r(/([^/]+\Z)), '/_\1')
-      end
-
-      target_view_path = full_view_paths.find do |view_path|
-        lookup_context.exists? view_path
-      end
-
-      if target_view_path
-        render target_view_path.gsub('/_', '/'), options
-      else
-        raise ActionView::MissingTemplate
-      end
+    def render options = {}, locals = {}, &block
+      view_path = File.dirname caller[0].gsub(%r(:.*\Z), '')
+      view_paths << view_path
+      super options, locals, &block
     end
 
     def ct *args
