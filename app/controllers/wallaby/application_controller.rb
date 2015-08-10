d3e9f2c43a910d72f730c74b05e271bd8c07513f
Tian Chen <me@tian.im>
2015-08-11 07:56:43 +1200

    make not_found play nice with resources_router

--- app/controllers/wallaby/application_controller.rb
+++ app/controllers/wallaby/application_controller.rb
@@ -1,7 +1,7 @@
 module Wallaby
   class ApplicationController < ::ApplicationController
     begin # error handling methods
-      def not_found exception
+      def not_found exception = nil
         render text: 'Not found', status: 404
       end
     end
