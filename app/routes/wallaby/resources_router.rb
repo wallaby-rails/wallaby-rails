9373ee172244fc1c55064d2822aef3949392d893
Tian Chen <me@tian.im>
2015-08-20 19:32:28 +1000

    refactor resources_router again

--- app/routes/wallaby/resources_router.rb
+++ app/routes/wallaby/resources_router.rb
@@ -1,21 +1,17 @@
 class Wallaby::ResourcesRouter
   def call env
-    target_controller = find_controller env
-    target_action     = params(env)[:action]
+    params            = env['action_dispatch.request.path_parameters']
+    target_controller = find_controller params[:resources]
+    target_action     = params[:action]
     target_controller.action(target_action).call env
   rescue AbstractController::ActionNotFound
     target_controller.action(:not_found).call env
   end
 
   protected
-  def find_controller env
-    resources_name = params(env)[:resources]
+  def find_controller resources_name
     Wallaby::ResourcesController.subclasses.find do |klass|
       klass.resources_name == resources_name
     end or Wallaby::ResourcesController
-  end
-
-  def params env
-    env['action_dispatch.request.path_parameters']
   end
 end
