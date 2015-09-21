2c5b51cba116e05e4eeae5ac9369b579d8a71984
Tian Chen <me@tian.im>
2015-09-22 00:42:30 +0800

    clean up for old decorators

--- app/services/wallaby/services/model_decorator_finder.rb
+++
@@ -1,8 +0,0 @@
-class Wallaby::Services::ModelDecoratorFinder
-  def self.find model_class
-    target_decorator_class = Wallaby::RecordDecorator.subclasses.find do |klass|
-      klass.model_class == model_class
-    end
-    target_decorator_class || Wallaby::ModelDecorator.new(model_class)
-  end
-end
