340120495cec67d6117b3cf12bb704a1dbbdc019
Tian Chen <me@tian.im>
2015-10-03 06:08:22 +0800

    filter all HABTM and schema migration tables

--- app/adaptors/wallaby/active_record/model_finder.rb
+++ app/adaptors/wallaby/active_record/model_finder.rb
@@ -2,7 +2,7 @@ class Wallaby::ActiveRecord::ModelFinder < Wallaby::ModelFinder
   protected
   def all
     ActiveRecord::Base.subclasses.reject do |model_class|
-      model_class.abstract_class? || [  ].include?(model_class)
+      model_class.abstract_class? || /ActiveRecord::SchemaMigration|HABTM/ =~ model_class.to_s
     end
   end
 end
