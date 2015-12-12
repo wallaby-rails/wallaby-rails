8a5e3fd13b17b96b915d2056690f32c964e33a74
Tian Chen <me@tian.im>
2015-12-13 00:18:44 +1100

    1. rename foreign_type and foreign_list to polymorphic
    2. change label for association fields
    3. update model_class

--- app/adaptors/wallaby/active_record/model_decorator/fields_builder.rb
+++ app/adaptors/wallaby/active_record/model_decorator/fields_builder.rb
@@ -19,17 +19,17 @@ class Wallaby::ActiveRecord::ModelDecorator::FieldsBuilder
       type = extract_type_from reflection
 
       fields[field_name] = {
-        name:           field_name,
-        type:           type,
-        label:          label_for(reflection, type),
-        is_association: true,
-        is_polymorphic: is_polymorphic?(reflection),
-        is_through:     is_through?(reflection),
-        has_scope:      has_scope?(reflection),
-        foreign_key:    foreign_key_for(reflection, type),
-        foreign_type:   foreign_type_for(reflection, type),
-        foreign_list:   foreign_list_for(reflection),
-        class:          class_for(reflection)
+        name:             field_name,
+        type:             type,
+        label:            field_name.titleize,
+        is_association:   true,
+        is_polymorphic:   is_polymorphic?(reflection),
+        is_through:       is_through?(reflection),
+        has_scope:        has_scope?(reflection),
+        foreign_key:      foreign_key_for(reflection, type),
+        polymorphic_type: polymorphic_type_for(reflection, type),
+        polymorphic_list: polymorphic_list_for(reflection),
+        class:            class_for(reflection)
       }
       fields
     end
@@ -45,7 +45,7 @@ class Wallaby::ActiveRecord::ModelDecorator::FieldsBuilder
   end
 
   def is_polymorphic?(reflection)
-    reflection.options[:polymorphic] || reflection.options[:as].present?
+    reflection.options[:polymorphic].present?
   end
 
   def extract_type_from(reflection)
@@ -57,30 +57,24 @@ class Wallaby::ActiveRecord::ModelDecorator::FieldsBuilder
     target.class.name.match(/([^:]+)Reflection/)[1].underscore
   end
 
-  def label_for(reflection, type)
-    label = Wallaby::Utils.to_model_label reflection.class_name
-    label = label.pluralize if many? type
-    label
-  end
-
   def foreign_key_for(reflection, type)
     foreign_key = reflection.association_foreign_key
     namespace   = reflection.class_name.split('::')[0...-1].map(&:underscore).join '_'
     foreign_key = "#{ namespace }_#{ foreign_key }" if namespace.present?
-    foreign_key = "#{ foreign_key }s" if many? type
+    foreign_key = "#{ foreign_key }s" if /many/ =~ type
     foreign_key
   end
 
-  def foreign_type_for(reflection, type)
+  def polymorphic_type_for(reflection, type)
     if is_polymorphic? reflection
       foreign_key = foreign_key_for reflection, type
       foreign_key.gsub %r(_ids?$), '_type'
     end
   end
 
-  def foreign_list_for(reflection)
+  def polymorphic_list_for(reflection)
     if is_polymorphic? reflection
-      all_model_class.inject([]) do |list, model_class|
+      available_model_class.inject([]) do |list, model_class|
         if model_defined_polymorphic_name? model_class, reflection.name
           list << model_class
         end
@@ -95,15 +89,11 @@ class Wallaby::ActiveRecord::ModelDecorator::FieldsBuilder
     end
   end
 
-  def all_model_class
-    Wallaby.adaptor.model_finder.new.available_model_classes
+  def available_model_class
+    Wallaby.configuration.adaptor.model_finder.new.available
   end
 
   def class_for(reflection)
     reflection.class_name.constantize if !is_polymorphic? reflection
-  end
-
-  def many?(type)
-    /many/ =~ type
   end
 end
