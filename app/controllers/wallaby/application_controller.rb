a67047cbccd9d4bc8db4a38328b8adda42c591f9
Tian Chen <me@tian.im>
2015-12-21 09:59:14 +1100

    1. add error handling for resource not found and model not found
    2. override _prefixes
    3. override lookup_context

--- app/controllers/wallaby/application_controller.rb
+++ app/controllers/wallaby/application_controller.rb
@@ -1,9 +1,19 @@
-module Wallaby
-  class ApplicationController < ::ApplicationController
-    begin # error handling methods
-      def not_found exception = nil
-        render text: 'Not found', status: 404
-      end
-    end
+class Wallaby::ApplicationController < ActionController::Base
+  rescue_from \
+    Wallaby::ResourceNotFound,
+    Wallaby::ModelNotFound,
+    with: :not_found
+
+  def not_found(exception = nil)
+    @exception = exception || Wallaby::OperationNotFound.new(params[:action])
+    render 'wallaby/errors/not_found', status: 404
+  end
+
+  def _prefixes
+    @_prefixes ||= Wallaby::PrefixesBuilder.new(self).build
+  end
+
+  def lookup_context
+    @_lookup_context ||= Wallaby::LookupContextWrapper.new super
   end
 end
---
+++ spec/controllers/wallaby/application_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe Wallaby::ApplicationController do
+  describe 'error handling' do
+    context 'Wallaby::ResourceNotFound' do
+      controller do
+        def index
+          raise Wallaby::ResourceNotFound.new('Product')
+        end
+      end
+
+      it 'rescues the exception and renders 404' do
+        expect{ get :index }.not_to raise_error
+        expect(response.status).to eq 404
+      end
+
+      it 'renders not_found view' do
+        expect(controller).to receive(:render).with('wallaby/errors/not_found', status: 404)
+        get :index
+      end
+    end
+  end
+
+  describe '#lookup_context' do
+    it 'returns a cacheing lookup_context' do
+      expect(controller.lookup_context).to be_a Wallaby::LookupContextWrapper
+      expect(controller.instance_variable_get :@_lookup_context).to be_a Wallaby::LookupContextWrapper
+    end
+  end
+
+  describe '_prefixes' do
+    it 'returns a new _prefixes' do
+      allow(controller).to receive(:params).and_return({ action: 'index' })
+      expect(controller._prefixes).to eq ["wallaby/application/index", "wallaby/application", ""]
+    end
+  end
+end
