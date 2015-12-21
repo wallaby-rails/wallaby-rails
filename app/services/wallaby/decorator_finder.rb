cdd4195ca20a2169e06d31d0b584eaa9a12f1426
Tian Chen <me@tian.im>
2015-12-21 17:47:19 +1100

    exclude anonymous classes for decorator_finder

--- app/services/wallaby/decorator_finder.rb
+++ app/services/wallaby/decorator_finder.rb
@@ -1,16 +1,16 @@
 class Wallaby::DecoratorFinder
-  def self.find_model model_class
+  def self.find_model(model_class)
     Wallaby::ResourceDecorator.try do |base|
       base.subclasses.find do |klass|
-        klass.model_class == model_class
+        klass.model_class == model_class && !(klass.to_s =~ /#<Class/)
       end || base.model_decorator(model_class)
     end
   end
 
-  def self.find_resource model_class
+  def self.find_resource(model_class)
     Wallaby::ResourceDecorator.try do |base|
       base.subclasses.find do |klass|
-        klass.model_class == model_class
+        klass.model_class == model_class && !(klass.to_s =~ /#<Class/)
       end || base
     end
   end
---
+++ spec/services/wallaby/decorator_finder_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+describe Wallaby::DecoratorFinder do
+  describe '.find_model' do
+    let(:model_class) { Category }
+
+    it 'returns Wallaby::ModelDecorator by default' do
+      model_decorator = described_class.find_model model_class
+      expect(model_decorator).to be_a Wallaby::ModelDecorator
+      expect(model_decorator.model_class).to eq model_class
+    end
+
+    context 'when there is anonymous sub class' do
+      it 'returns Wallaby::ModelDecorator' do
+        decorator_class = Class.new(Wallaby::ResourceDecorator) do
+          def self.model_class
+            Category
+          end
+        end
+        expect(Wallaby::ResourceDecorator.subclasses).to include decorator_class
+        model_decorator = described_class.find_model model_class
+        expect(model_decorator).to be_a Wallaby::ModelDecorator
+        expect(model_decorator.model_class).to eq model_class
+      end
+    end
+
+    context 'when there is a sub class' do
+      let!(:decorator_class) do
+        class CategoryDecorator < Wallaby::ResourceDecorator; end
+        CategoryDecorator
+      end
+      it 'returns sub classes' do
+        expect(Wallaby::ResourceDecorator.subclasses).to include decorator_class
+        model_decorator = described_class.find_model model_class
+        expect(model_decorator).to eq decorator_class
+        expect(model_decorator.model_class).to eq model_class
+      end
+    end
+  end
+
+  describe '.find_resource' do
+    let(:model_class) { Order }
+
+    it 'returns Wallaby::ResourceDecorator by default' do
+      model_decorator = described_class.find_resource model_class
+      expect(model_decorator).to eq Wallaby::ResourceDecorator
+    end
+
+    context 'when there is anonymous sub class' do
+      it 'returns Wallaby::ResourceDecorator' do
+        decorator_class = Class.new(Wallaby::ResourceDecorator) do
+          def self.model_class
+            Order
+          end
+        end
+        expect(Wallaby::ResourceDecorator.subclasses).to include decorator_class
+        model_decorator = described_class.find_resource model_class
+        expect(model_decorator).to eq Wallaby::ResourceDecorator
+      end
+    end
+
+    context 'when there is a sub class' do
+      let!(:decorator_class) do
+        class OrderDecorator < Wallaby::ResourceDecorator; end
+        OrderDecorator
+      end
+      it 'returns sub classes' do
+        expect(Wallaby::ResourceDecorator.subclasses).to include decorator_class
+        model_decorator = described_class.find_resource model_class
+        expect(model_decorator).to eq decorator_class
+        expect(model_decorator.model_class).to eq model_class
+      end
+    end
+  end
+end
