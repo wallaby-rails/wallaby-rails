f4bbb615f1f2a58cc5e687582b66cedcf8f28b19
Tian Chen <me@tian.im>
2016-02-02 19:01:26 +1100

    add styling helper

---
+++ app/helpers/wallaby/styling_helper.rb
@@ -0,0 +1,48 @@
+module Wallaby::StylingHelper
+  def icon(icon_suffix, html_options = {})
+    html_options[:class] = Array html_options[:class]
+    html_options[:class] << "glyphicon glyphicon-#{ icon_suffix }"
+
+    content_tag :i, nil, html_options
+  end
+
+  def itooltip(title, icon_suffix = 'info-sign')
+    icon icon_suffix, title: title, data: { toggle: "tooltip", placement: "top" }
+  end
+
+  def ilink_to(options = nil, html_options = {})
+    icon_suffix = html_options.delete(:icon) || 'info-sign'
+    link_to options, html_options do
+      icon icon_suffix
+    end
+  end
+
+  def open_model(title, body, label = nil)
+    uuid = random_uuid
+    label ||= icon 'circle-arrow-up'
+    link_to(label, 'javascript:;', data: { toggle: 'modal', target: "##{ uuid }" }) +
+    content_tag(:div, id: uuid, class: 'modal fade', tabindex: -1, role: 'dialog') do
+      content_tag :div, class: 'modal-dialog modal-lg' do
+        content_tag :div, class: 'modal-content' do
+          content_tag(:div, class: 'modal-header') do
+            button_tag(type: 'button', class: 'close', data: { dismiss: 'modal' }, aria: { label: 'Close' }) do
+              content_tag :span, raw('&times;'), aria: { hidden: true }
+            end +
+            content_tag(:h4, title, class: 'modal-title')
+          end +
+          content_tag(:div, class: 'modal-body') do
+            body
+          end
+        end
+      end
+    end
+  end
+
+  def null
+    content_tag :i, '<null>', class: 'text-muted'
+  end
+
+  def na
+    content_tag :i, '<n/a>', class: 'text-muted'
+  end
+end
