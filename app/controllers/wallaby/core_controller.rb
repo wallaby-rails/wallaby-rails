adccb74c154261781bfc70059c05cf4da6245382
Tian Chen <me@tian.im>
2016-02-18 20:38:22 +0800

    1. add current_resources_name and current_model_class to core_controller
    2. remove core_methods

--- app/controllers/wallaby/core_controller.rb
+++ app/controllers/wallaby/core_controller.rb
@@ -1,13 +1,28 @@
 module Wallaby
   class CoreController < SecureController
-    include CoreMethods
-    helper 'wallaby/styling'
-    helper 'wallaby/links'
-
     before_action :authenticate_user!, except: [ :status ]
 
     def status
       render text: 'healthy'
+    end
+
+    protected
+    begin # helper methods
+      helper_method \
+        :current_resources_name,
+        :current_model_class
+
+      def current_resources_name
+        @current_resources_name ||= if self.class.respond_to? :resources_name
+          self.class.resources_name
+        end || params[:resources]
+      end
+
+      def current_model_class
+        @current_model_class ||= if self.class.respond_to? :model_class
+          self.class.model_class
+        end || Wallaby::Utils.to_model_class(current_resources_name)
+      end
     end
   end
 end
--- spec/controllers/wallaby/core_controller_spec.rb
+++ spec/controllers/wallaby/core_controller_spec.rb
@@ -10,4 +10,36 @@ describe Wallaby::CoreController do
       expect(response.body).to eq 'healthy'
     end
   end
+
+  describe '#current_resources_name' do
+    it 'returns resources_name from params' do
+      allow(controller).to receive(:params).and_return({ resources: 'on_sale_products' })
+      expect(controller.send :current_resources_name).to eq 'on_sale_products'
+    end
+
+    context 'when it responds to resources_name' do
+      it 'returns resources_name from controller class' do
+        klass = double resources_name: 'products', superclass: Object
+        allow(controller).to receive(:class).and_return klass
+        allow(controller).to receive(:params).and_return({ resources: 'on_sale_products' })
+        expect(controller.send :current_resources_name).to eq 'products'
+      end
+    end
+  end
+
+  describe '#current_model_class' do
+    it 'returns model class from current_resources_name' do
+      allow(controller).to receive(:current_resources_name) { 'products' }
+      expect(controller.send :current_model_class).to eq Product
+    end
+
+    context 'when it responds to model_class' do
+      it 'returns model_class from controller class' do
+        klass = double model_class: Product, superclass: Object
+        allow(controller).to receive(:class) { klass }
+        allow(controller).to receive(:current_resources_name) { 'categories' }
+        expect(controller.send :current_model_class).to eq Product
+      end
+    end
+  end
 end
