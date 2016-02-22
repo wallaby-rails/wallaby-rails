933d1c45240213d7795a6df228de5f88d2f2280a
Tian Chen <me@tian.im>
2016-02-22 12:19:05 +0800

    remove ActiveRecord::SchemaMigrationDecorator

--- spec/dummy/app/decorators/active_record/schema_migration_decorator.rb
+++
@@ -1,9 +0,0 @@
-class ActiveRecord::SchemaMigrationDecorator < Wallaby::ResourceDecorator
-  def self.find_or_initialize id
-    model_class.where(version: id).first_or_initialize
-  end
-
-  def to_param
-    resource.version
-  end
-end
