28bd626ed77e605570b243804a1adee38373c5ad
Tian Chen <me@tian.im>
2016-02-22 12:04:13 +0800

    make sure that reject anonymous class

--- app/adaptors/wallaby/active_record/model_finder.rb
+++ app/adaptors/wallaby/active_record/model_finder.rb
@@ -3,9 +3,9 @@ class Wallaby::ActiveRecord::ModelFinder < Wallaby::ModelFinder
   def all
     Rails.cache.fetch 'wallaby/model_finder' do
       ActiveRecord::Base.subclasses.reject do |model_class|
-        model_class.name.blank? ||
         model_class.abstract_class? ||
-        /ActiveRecord::SchemaMigration|HABTM/ =~ model_class.name
+        (/ActiveRecord::SchemaMigration|HABTM/ =~ model_class.name) ||
+        (/\A#<Class/ =~ model_class.to_s) # anonymouse class
       end.sort_by &:to_s
     end
   end
