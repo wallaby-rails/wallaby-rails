906c290b4b37e4ff9e3225c9c4c8e266b8106d5a
Tian Chen <me@tian.im>
2016-03-06 23:04:29 -0500

    update lookup context

--- app/services/wallaby/lookup_context_wrapper.rb
+++ app/services/wallaby/lookup_context_wrapper.rb
@@ -1,11 +1,8 @@
 class Wallaby::LookupContextWrapper
-  def self.original_methods
-    ActionView::LookupContext.public_instance_methods - \
-      Object.instance_methods - \
-      %i( find_template )
-  end
-
-  delegate *original_methods, to: :@lookup_context
+  delegate *begin # origin methods
+    ActionView::LookupContext.instance_methods \
+      - Object.instance_methods - %i( find_template )
+  end, to: :@lookup_context
 
   def initialize(lookup_context)
     @lookup_context = lookup_context
@@ -21,29 +18,23 @@ class Wallaby::LookupContextWrapper
   protected
   def caching(key)
     @templates ||= {}
-    if ! @templates.has_key? key
+    unless @templates.has_key? key
       @templates[key] = begin
         yield
-      rescue ActionView::MissingTemplate => e
-        if Rails.env.development?
-          raise
-        else
-          BlankTemplate.new
-        end
+      rescue ActionView::MissingTemplate
+        raise if Rails.env.development?
+        BlankTemplate.new
       end
     end
     @templates[key]
   end
 
-  class BlankTemplate
-    def render *args
+  class BlankTemplate < ActionView::Template::HTML
+    def initialize
+      super nil
     end
 
-    def identifier
-    end
-
-    def formats
-      [ :html ]
+    def render(*args)
     end
   end
 end
--- spec/services/wallaby/lookup_context_wrapper_spec.rb
+++ spec/services/wallaby/lookup_context_wrapper_spec.rb
@@ -7,9 +7,10 @@ describe Wallaby::LookupContextWrapper do
   describe '.find_template' do
     context 'when template exists' do
       it 'caches the result for original lookup_context' do
-        allow(original_lookup_context).to receive :find_template
+        expect(original_lookup_context).to receive :find_template
         subject.find_template 'target'
         expect(subject.instance_variable_get(:@templates)).to have_key 'target'
+        subject.find_template 'target'
       end
     end
 
@@ -17,17 +18,6 @@ describe Wallaby::LookupContextWrapper do
       it 'returns blank template' do
         allow(original_lookup_context).to receive(:find_template).and_raise(ActionView::MissingTemplate.new ['paths'], 'path', ['prefixes'], 'partial', 'details')
         expect(subject.find_template 'target').to be_kind_of Wallaby::LookupContextWrapper::BlankTemplate
-      end
-    end
-  end
-
-  describe 'all instance methods' do
-    it 'delegates all methods to original lookup_context' do
-      described_class.original_methods.each do |method_id|
-        args_size = original_lookup_context.method(method_id).arity
-        args_size = - (args_size + 1) if args_size < 0
-        expect(original_lookup_context).to receive method_id
-        subject.send method_id, *(0...args_size)
       end
     end
   end
