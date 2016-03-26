945ddb1ccae81468a8c4b454564d611715600d25
Tian Chen <me@tian.im>
2016-03-26 17:41:20 -0400

    update edit/new page to add form js/css

---
+++ app/assets/javascripts/wallaby/form.js
@@ -0,0 +1,7 @@
+//= require moment
+//= require bootstrap-datetimepicker
+//= require summernote
+//= require codemirror
+//= require codemirror/modes/xml
+//= require codemirror/modes/ruby
+//= require codemirror/modes/javascript
