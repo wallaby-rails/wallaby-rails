04bcab39919ecc092ddb77098a0ad38d59ed2769
Tian Chen <me@tian.im>
2016-03-30 12:11:37 -0400

    update model_servicer with collection/new/find methods

--- app/adaptors/wallaby/active_record/model_servicer.rb
+++ app/adaptors/wallaby/active_record/model_servicer.rb
@@ -1,25 +1,137 @@
 class Wallaby::ActiveRecord::ModelServicer
-  def initialize(model_class)
-    @model_class = model_class
+  def initialize(model_class, model_decorator = nil)
+    fail ArgumentError, 'model class required' unless model_class
+    @model_class      = model_class
+    @model_decorator  = model_decorator || Wallaby::DecoratorFinder.find_model(@model_class)
+  end
+
+  def collection(params)
+    @model_decorator.collection params
+  end
+
+  def new(params)
+    permitted = permit(params) rescue {}
+    @model_class.new permitted
+  end
+
+  def find(id, params)
+    @model_class.find id
+  rescue ActiveRecord::RecordNotFound
+    fail Wallaby::ResourceNotFound
   end
 
   def create(params)
     resource = @model_class.new
-    resource.assign_attributes params
-    resource.save
+    resource.assign_attributes normalize permit(params)
+    resource.save if valid? resource
     [ resource, resource.errors.blank? ]
+  rescue ActiveRecord::StatementInvalid => e
+    resource.errors.add :base, e.message
+    [ resource, false ]
   end
 
   def update(id, params)
     resource = @model_class.find id
-    resource.assign_attributes params
-    resource.save
+    resource.assign_attributes normalize permit(params)
+    resource.save if valid? resource
     [ resource, resource.errors.blank? ]
   rescue ActiveRecord::RecordNotFound
-    [ nil, false ]
+    fail Wallaby::ResourceNotFound
+  rescue ActiveRecord::StatementInvalid => e
+    resource.errors.add :base, e.message
+    [ resource, false ]
   end
 
-  def destroy(id)
+  def destroy(id, params)
     @model_class.delete id
+  end
+
+  protected
+  def permit(params)
+    params.require(param_key).permit(simple_field_names << compound_hashed_fields)
+  end
+
+  def normalize(params)
+    params.each do |field_name, values|
+      metadata = @model_decorator.fields[field_name]
+      next unless metadata
+      type = metadata[:type]
+
+      case type
+      when /range/
+        if values.is_a?(Array) && values.size == 2
+          params[field_name] = values.first...values.last
+        end
+      when /point/
+        params[field_name] = values.map &:to_f
+      when /binary/
+        if values.is_a?(ActionDispatch::Http::UploadedFile)
+          params[field_name] = values.read
+        end
+      end
+    end
+  end
+
+  def valid?(resource)
+    resource.attributes.each do |field_name, values|
+      metadata = @model_decorator.fields[field_name]
+      if /(date|ts|tstz)range/ =~ metadata[:type] && values.try(:any?, &:blank?)
+        resource.errors.add field_name, 'required for range data'
+      end
+    end
+    resource.errors.blank?
+  end
+
+  def param_key
+    @model_class.model_name.param_key
+  end
+
+  def simple_field_names
+    non_range_fields.keys + belongs_to_fields.map do |_, metadata|
+      [ metadata[:foreign_key], metadata[:polymorphic_type] ].compact
+    end.flatten
+  end
+
+  def compound_hashed_fields
+    field_names = range_fields.keys +
+      point_fields.keys +
+      many_association_fields.map{ |_, metadata| metadata[:foreign_key] }
+    field_names.map{ |field_name| [ field_name, [] ] }.to_h
+  end
+
+  def non_association_fields
+    @model_decorator.fields.select{ |_, metadata| !metadata[:is_association] }
+  end
+
+  def non_range_fields
+    non_association_fields.select{ |_, metadata| !(/range|point/ =~ metadata[:type]) }
+  end
+
+  def range_fields
+    non_association_fields.select do |_, metadata|
+      /range|point/ =~ metadata[:type]
+    end
+  end
+
+  def point_fields
+    non_association_fields.select do |_, metadata|
+      /point/ =~ metadata[:type]
+    end
+  end
+
+  def association_fields
+    @model_decorator.fields.select do |_, metadata|
+      metadata[:is_association] && !metadata[:has_scope] && !metadata[:is_through]
+    end
+  end
+
+  def many_association_fields
+    association_fields.select{ |_, metadata| /many/ =~ metadata[:type] }
+  end
+
+  def belongs_to_fields
+    association_fields.select do |field_name, metadata|
+      'belongs_to' == metadata[:type]
+    end
   end
 end
--- spec/adaptors/wallaby/active_record/model_servicer_spec.rb
+++ spec/adaptors/wallaby/active_record/model_servicer_spec.rb
@@ -2,42 +2,121 @@ require 'rails_helper'
 
 describe Wallaby::ActiveRecord::ModelServicer do
   describe 'actions' do
-    subject { described_class.new Product }
+    subject { described_class.new model_class, model_decorator }
+    let(:model_class) { AllPostgresType }
+    let(:model_decorator) { Wallaby::ActiveRecord::ModelDecorator.new model_class }
+
+    describe '#collection' do
+      it 'returns the collection' do
+        expect(model_decorator).to receive(:collection) { [] }
+        subject.collection({})
+      end
+    end
+
+    describe '#new' do
+      it 'returns a resource' do
+        resource = subject.new ActionController::Parameters.new({})
+        expect(resource).to be_a model_class
+        expect(resource.attributes.values.compact).to be_blank
+
+        resource = subject.new ActionController::Parameters.new(string: 'some string')
+        expect(resource).to be_a model_class
+        expect(resource.attributes.values.compact).to be_blank
+
+        resource = subject.new ActionController::Parameters.new(all_postgres_type: { string: 'some string' })
+        expect(resource).to be_a model_class
+        expect(resource.attributes.values.compact).not_to be_blank
+        expect(resource.string).to eq 'some string'
+      end
+    end
+
+    describe '#find' do
+      it 'returns a resource' do
+        existing = model_class.create({})
+        resource = nil
+        expect{ resource = subject.find existing.id, ActionController::Parameters.new({}) }.not_to raise_error
+        expect(resource).to be_a model_class
+      end
+
+      context 'when it is not found' do
+        it 'raises error' do
+          expect{ subject.find 0, ActionController::Parameters.new({}) }.to raise_error Wallaby::ResourceNotFound
+        end
+      end
+    end
 
     describe '#create' do
       it 'returns the resource and is_success' do
-        expect_any_instance_of(Product).to receive :save
-        resource, is_success = subject.create(sku: 'sku123')
-        expect(resource.sku).to eq 'sku123'
-        expect(resource.errors).to be_blank
+        resource, is_success = subject.create ActionController::Parameters.new(all_postgres_type: { string: 'string' })
+        expect(resource).to be_a model_class
+        expect(resource.id).not_to be_blank
         expect(is_success).to be_truthy
+      end
+
+      context 'when params are not valid' do
+        it 'returns the resource and is_failed' do
+          resource, is_success = subject.create ActionController::Parameters.new(all_postgres_type: { daterange: [ '', '2016-12-13' ] })
+          expect(resource).to be_a model_class
+          expect(resource.id).to be_blank
+          expect(resource.errors).not_to be_blank
+          expect(is_success).to be_falsy
+        end
+      end
+
+      context 'when database throws error' do
+        it 'returns the resource and is_failed' do
+          expect_any_instance_of(model_class).to receive(:save) { raise ActiveRecord::StatementInvalid, 'StatementInvalid' }
+          resource, is_success = subject.create ActionController::Parameters.new(all_postgres_type: { string: 'string' })
+          expect(resource).to be_a model_class
+          expect(resource.id).to be_blank
+          expect(resource.errors).not_to be_blank
+          expect(resource.errors[:base]).to eq [ 'StatementInvalid' ]
+          expect(is_success).to be_falsy
+        end
       end
     end
 
     describe '#update' do
+      let!(:existing) { model_class.create string: 'title' }
       it 'returns the resource and is_success' do
-        product = Product.new
-        expect(Product).to receive(:find).and_return product
-        expect(product).to receive(:save)
-        resource, is_success = subject.update(1, sku: 'sku123')
-        expect(resource.sku).to eq 'sku123'
-        expect(resource.errors).to be_blank
+        resource, is_success = subject.update existing.id, ActionController::Parameters.new(all_postgres_type: { string: 'string' })
+        expect(resource).to be_a model_class
+        expect(resource.string).to eq 'string'
         expect(is_success).to be_truthy
       end
 
-      context 'when resource is found' do
-        it 'returns nil and false' do
-          resource, is_success = subject.update(1, sku: 'sku123')
-          expect(resource).to be_nil
+      context 'when resource not found' do
+        it 'raises Wallaby::ResourceNotFound' do
+          expect{ subject.update 0, ActionController::Parameters.new(all_postgres_type: { daterange: [ '', '2016-12-13' ] }) }.to raise_error Wallaby::ResourceNotFound
+        end
+      end
+
+      context 'when params are not valid' do
+        it 'returns the resource and is_failed' do
+          resource, is_success = subject.update existing.id, ActionController::Parameters.new(all_postgres_type: { daterange: [ '', '2016-12-13' ] })
+          expect(resource).to be_a model_class
+          expect(resource.errors).not_to be_blank
+          expect(is_success).to be_falsy
+        end
+      end
+
+      context 'when database throws error' do
+        it 'returns the resource and is_failed' do
+          expect_any_instance_of(model_class).to receive(:save) { raise ActiveRecord::StatementInvalid, 'StatementInvalid' }
+          resource, is_success = subject.update existing.id, ActionController::Parameters.new(all_postgres_type: { string: 'string' })
+          expect(resource).to be_a model_class
+          expect(resource.errors).not_to be_blank
+          expect(resource.errors[:base]).to eq [ 'StatementInvalid' ]
           expect(is_success).to be_falsy
         end
       end
     end
 
     describe '#destroy' do
-      it 'calls delete' do
-        expect(Product).to receive(:delete)
-        subject.destroy 1
+      it 'returns is_success regardless whether the record exists' do
+        expect(subject.destroy 0, {}).to be_truthy
+        existing = model_class.create({})
+        expect(subject.destroy existing.id, {}).to be_truthy
       end
     end
   end
