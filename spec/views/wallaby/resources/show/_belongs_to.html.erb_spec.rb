6fcbc1fb90ee1a62347836c33c85b6a284303884
Tian Chen <me@tian.im>
2016-03-30 12:22:12 -0400

    update belongs_to page to show for polymorphic value

--- spec/views/wallaby/resources/show/_belongs_to.html.erb_spec.rb
+++ spec/views/wallaby/resources/show/_belongs_to.html.erb_spec.rb
@@ -8,13 +8,20 @@ describe 'partial' do
   before { render partial, value: value, metadata: metadata }
 
   it 'renders the belongs_to' do
-    expect(rendered).to eq "<a href=\"/admin/products/1\">Hiking shoes</a>\n"
+    expect(rendered).to eq "  <a href=\"/admin/products/1\">Hiking shoes</a>\n"
   end
 
   context 'when value is nil' do
     let(:value) { nil }
     it 'renders new_link' do
-      expect(rendered).to eq "<a class=\"text-success\" href=\"/admin/products/new\">Create Product</a>\n"
+      expect(rendered).to eq "  <a class=\"text-success\" href=\"/admin/products/new\">Create Product</a>\n"
+    end
+
+    context 'when value is polymorphic' do
+      let(:metadata) { Hash is_polymorphic: true }
+      it 'renders new_link' do
+        expect(rendered).to eq "  <i class=\"text-muted\">&lt;null&gt;</i>\n"
+      end
     end
   end
 end
