15b529badda08a7142d565eb126a9bd1170562c3
Tian Chen <me@tian.im>
2016-03-31 22:49:40 -0400

    add cancan to all the link helpers, and update the htmls that use these link helpers

--- app/helpers/wallaby/links_helper.rb
+++ app/helpers/wallaby/links_helper.rb
@@ -21,13 +21,16 @@ module Wallaby::LinksHelper
 
   def index_link(model_class = nil, html_options = {}, &block)
     model_class ||= current_model_class
+    return if cannot? :index, model_class
     block ||= -> { to_model_label model_class }
 
     link_to index_path(model_class), html_options, &block
   end
 
   def new_link(model_class = nil, html_options = {}, &block)
-    block ||= -> { ct 'link.new', model: to_model_label(model_class) }
+    model_class ||= current_model_class
+    return if cannot? :new, model_class
+    block ||= -> { "#{ ct 'link.new' } #{ to_model_label model_class }" }
     html_options[:class] = 'text-success' unless html_options.has_key? :class
 
     prepend_if html_options
@@ -35,6 +38,7 @@ module Wallaby::LinksHelper
   end
 
   def show_link(resource, html_options = {}, &block)
+    return if cannot? :show, resource
     # NOTE: to_s is a must
     # if a block is returning integer (e.g. `{ 1 }`)
     # `link_to` will render blank text note inside hyper link
@@ -44,6 +48,7 @@ module Wallaby::LinksHelper
   end
 
   def edit_link(resource, html_options = {}, &block)
+    return if cannot? :edit, resource
     block ||= -> { "#{ ct 'link.edit' } #{ decorate(resource).to_label }" }
     html_options[:class] = 'text-warning' unless html_options.has_key? :class
 
@@ -51,6 +56,9 @@ module Wallaby::LinksHelper
   end
 
   def delete_link(resource, html_options = {}, &block)
+    return if cannot? :destroy, resource
+    block ||= -> { ct 'link.delete' }
+    html_options[:class]  = 'text-danger' unless html_options.has_key? :class
     html_options[:method] ||= :delete
     html_options[:data]   ||= {}
     html_options[:data][:confirm] ||= ct('link.confirm.delete')
@@ -58,9 +66,9 @@ module Wallaby::LinksHelper
     link_to show_path(resource), html_options, &block
   end
 
-  def cancel_link(html_options = {})
-    label = html_options.delete(:label) || ct('link.cancel')
-    link_to label, :back, html_options
+  def cancel_link(html_options = {}, &block)
+    block ||= -> { ct 'link.cancel' }
+    link_to :back, html_options, &block
   end
 
   def prepend_if(html_options = {})
--- spec/helpers/wallaby/links_helper_spec.rb
+++ spec/helpers/wallaby/links_helper_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe Wallaby::LinksHelper do
   extend Wallaby::ApplicationHelper
 
+  before { allow(controller).to receive :current_user }
+
   describe '#index_path' do
     it 'returns index path' do
       expect(helper.index_path Product).to match '/products'
@@ -32,38 +34,86 @@ describe Wallaby::LinksHelper do
       expect(helper.index_link(Product)).to eq "<a href=\"/admin/products\">Product</a>"
       expect(helper.index_link(Product) { 'List' }).to eq "<a href=\"/admin/products\">List</a>"
     end
+
+    context 'when cannot index' do
+      it 'returns nil' do
+        expect(helper).to receive(:cannot?) { true }
+        expect(helper.index_link Product).to be_nil
+      end
+    end
   end
 
   describe '#new_link' do
     it 'returns new link' do
       expect(helper.new_link Product).to eq "<a class=\"text-success\" href=\"/admin/products/new\">Create Product</a>"
       expect(helper.new_link(Product) { 'New' }).to eq "<a class=\"text-success\" href=\"/admin/products/new\">New</a>"
+      expect(helper.new_link(Product, class: 'test')).to eq "<a class=\"test\" href=\"/admin/products/new\">Create Product</a>"
+    end
+
+    context 'when cannot new' do
+      it 'returns nil' do
+        expect(helper).to receive(:cannot?) { true }
+        expect(helper.new_link Product).to be_nil
+      end
     end
   end
 
   describe '#show_link' do
+    let(:resource) { Product.new id: 1 }
+
     it 'returns show link' do
-      expect(helper.show_link(Product.new id: 1)).to eq "<a href=\"/admin/products/1\">1</a>"
-      expect(helper.show_link(Product.new id: 1) { 'Show' }).to eq "<a href=\"/admin/products/1\">Show</a>"
+      expect(helper.show_link(resource)).to eq "<a href=\"/admin/products/1\">1</a>"
+      expect(helper.show_link(resource) { 'Show' }).to eq "<a href=\"/admin/products/1\">Show</a>"
+    end
+
+    context 'when cannot show' do
+      it 'returns nil' do
+        expect(helper).to receive(:cannot?) { true }
+        expect(helper.show_link resource ).to be_nil
+      end
     end
   end
 
   describe '#edit_link' do
+    let(:resource) { Product.new id: 1 }
+
     it 'returns edit link' do
-      expect(helper.edit_link(Product.new id: 1)).to eq "<a class=\"text-warning\" href=\"/admin/products/1/edit\">Edit 1</a>"
-      expect(helper.edit_link(Product.new id: 1) { 'Edit' }).to eq "<a class=\"text-warning\" href=\"/admin/products/1/edit\">Edit</a>"
+      expect(helper.edit_link(resource)).to eq "<a class=\"text-warning\" href=\"/admin/products/1/edit\">Edit 1</a>"
+      expect(helper.edit_link(resource) { 'Edit' }).to eq "<a class=\"text-warning\" href=\"/admin/products/1/edit\">Edit</a>"
+      expect(helper.edit_link(resource, class: 'test')).to eq "<a class=\"test\" href=\"/admin/products/1/edit\">Edit 1</a>"
+    end
+
+    context 'when cannot edit' do
+      it 'returns nil' do
+        expect(helper).to receive(:cannot?) { true }
+        expect(helper.edit_link resource ).to be_nil
+      end
     end
   end
 
   describe '#delete_link' do
+    let(:resource) { Product.new id: 1 }
+
     it 'returns delete link' do
-      expect(helper.delete_link(Product.new id: 1) { 'Delete' }).to eq "<a data-confirm=\"Please confirm to delete\" rel=\"nofollow\" data-method=\"delete\" href=\"/admin/products/1\">Delete</a>"
+      expect(helper.delete_link(resource)).to eq "<a class=\"text-danger\" data-confirm=\"Please confirm to delete\" rel=\"nofollow\" data-method=\"delete\" href=\"/admin/products/1\">Delete</a>"
+      expect(helper.delete_link(resource) { 'Destroy' }).to eq "<a class=\"text-danger\" data-confirm=\"Please confirm to delete\" rel=\"nofollow\" data-method=\"delete\" href=\"/admin/products/1\">Destroy</a>"
+      expect(helper.delete_link(resource, class: 'test')).to eq "<a class=\"test\" data-confirm=\"Please confirm to delete\" rel=\"nofollow\" data-method=\"delete\" href=\"/admin/products/1\">Delete</a>"
+      expect(helper.delete_link(resource, method: :put)).to eq "<a class=\"text-danger\" data-confirm=\"Please confirm to delete\" rel=\"nofollow\" data-method=\"put\" href=\"/admin/products/1\">Delete</a>"
+      expect(helper.delete_link(resource, data: { confirm: 'Delete now!' })).to eq "<a data-confirm=\"Delete now!\" class=\"text-danger\" rel=\"nofollow\" data-method=\"delete\" href=\"/admin/products/1\">Delete</a>"
+    end
+
+    context 'when cannot delete' do
+      it 'returns nil' do
+        expect(helper).to receive(:cannot?) { true }
+        expect(helper.delete_link resource ).to be_nil
+      end
     end
   end
 
   describe '#cancel_link' do
     it 'returns cancel link' do
       expect(helper.cancel_link).to eq "<a href=\"javascript:history.back()\">Cancel</a>"
+      expect(helper.cancel_link { 'Back' }).to eq "<a href=\"javascript:history.back()\">Back</a>"
     end
   end
 
