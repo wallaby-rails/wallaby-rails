d916291da58b8234b3fba320eecac04e15f6a6dc
Tian Chen <me@tian.im>
2016-04-06 08:49:31 -0400

    added permitter for model_servicer

---
+++ app/adaptors/wallaby/active_record/model_servicer/permitter.rb
@@ -0,0 +1,50 @@
+class Wallaby::ActiveRecord::ModelServicer::Permitter
+  def initialize(model_decorator)
+    @model_decorator = model_decorator
+  end
+
+  def simple_field_names
+    field_names = non_range_fields.keys + belongs_to_fields.map do |_, metadata|
+      [ metadata[:foreign_key], metadata[:polymorphic_type] ]
+    end.flatten.compact
+    field_names.reject do |field_name|
+      [ @model_decorator.primary_key, 'created_at', 'updated_at' ].include? field_name
+    end
+  end
+
+  def compound_hashed_fields
+    field_names = range_fields.keys + many_association_fields.map{ |_, metadata| metadata[:foreign_key] }
+    field_names.map{ |field_name| [ field_name, [] ] }.to_h
+  end
+
+  protected
+  def non_association_fields
+    @model_decorator.fields.select{ |_, metadata| !metadata[:is_association] }
+  end
+
+  def non_range_fields
+    non_association_fields.select{ |_, metadata| !(/range|point/ =~ metadata[:type]) }
+  end
+
+  def range_fields
+    non_association_fields.select do |_, metadata|
+      /range|point/ =~ metadata[:type]
+    end
+  end
+
+  def association_fields
+    @model_decorator.fields.select do |_, metadata|
+      metadata[:is_association] && !metadata[:has_scope] && !metadata[:is_through]
+    end
+  end
+
+  def many_association_fields
+    association_fields.select{ |_, metadata| /many/ =~ metadata[:type] }
+  end
+
+  def belongs_to_fields
+    association_fields.select do |field_name, metadata|
+      'belongs_to' == metadata[:type]
+    end
+  end
+end
---
+++ spec/adaptors/wallaby/active_record/model_servicer/permitter_spec.rb
@@ -0,0 +1,122 @@
+require 'rails_helper'
+
+describe Wallaby::ActiveRecord::ModelServicer::Permitter do
+  subject { described_class.new model_decorator }
+  let(:model_decorator) { Wallaby::ActiveRecord::ModelDecorator.new AllPostgresType }
+
+  describe 'model that has all postgres types' do
+    describe '#simple_field_names' do
+      it 'returns non-range fields' do
+        expect(subject.simple_field_names).to eq [ "string", "text", "integer", "float", "decimal", "datetime", "time", "date", "binary", "boolean", "bigint", "xml", "tsvector", "hstore", "inet", "cidr", "macaddr", "uuid", "json", "jsonb", "ltree", "citext", "bit", "bit_varying", "money" ]
+      end
+    end
+
+    describe '#compound_hashed_fields' do
+      it 'returns range fields' do
+        expect(subject.compound_hashed_fields).to eq "daterange" => [], "numrange" => [], "tsrange" => [], "tstzrange" => [], "int4range" => [], "int8range" => [], "point" => []
+      end
+    end
+
+    describe '#non_association_fields' do
+      it 'returns non-association fields' do
+        expect(subject.send(:non_association_fields).keys).to eq [ "id", "string", "text", "integer", "float", "decimal", "datetime", "time", "date", "daterange", "numrange", "tsrange", "tstzrange", "int4range", "int8range", "binary", "boolean", "bigint", "xml", "tsvector", "hstore", "inet", "cidr", "macaddr", "uuid", "json", "jsonb", "ltree", "citext", "point", "bit", "bit_varying", "money" ]
+      end
+    end
+
+    describe '#non_range_fields' do
+      it 'returns non-range fields' do
+        expect(subject.send(:non_range_fields).keys).to eq [ "id", "string", "text", "integer", "float", "decimal", "datetime", "time", "date", "binary", "boolean", "bigint", "xml", "tsvector", "hstore", "inet", "cidr", "macaddr", "uuid", "json", "jsonb", "ltree", "citext", "bit", "bit_varying", "money" ]
+      end
+    end
+
+    describe '#range_fields' do
+      it 'returns range fields' do
+        expect(subject.send(:range_fields).keys).to eq [ "daterange", "numrange", "tsrange", "tstzrange", "int4range", "int8range", "point" ]
+      end
+    end
+
+    describe '#association_fields' do
+      it 'returns association fields' do
+        expect(subject.send(:association_fields).keys).to eq []
+      end
+    end
+
+    describe '#many_association_fields' do
+      it 'returns many-association fields' do
+        expect(subject.send(:many_association_fields).keys).to eq []
+      end
+    end
+
+    describe '#belongs_to_fields' do
+      it 'returns belongs-to fields' do
+        expect(subject.send(:belongs_to_fields).keys).to eq []
+      end
+    end
+  end
+
+  describe 'model that has associations' do
+    let(:model_decorator) { Wallaby::ActiveRecord::ModelDecorator.new Product }
+    describe '#simple_field_names' do
+      it 'returns non-range fields' do
+        expect(subject.simple_field_names).to eq [ "sku", "name", "description", "stock", "price", "featured", "available_to_date", "available_to_time", "published_at", "category_id" ]
+      end
+    end
+
+    describe '#compound_hashed_fields' do
+      it 'returns range fields' do
+        expect(subject.compound_hashed_fields).to eq "item_ids" => [], "tag_ids" => []
+      end
+    end
+
+    describe '#non_association_fields' do
+      it 'returns non-association fields' do
+        expect(subject.send(:non_association_fields).keys).to eq [ "id", "sku", "name", "description", "stock", "price", "featured", "available_to_date", "available_to_time", "published_at" ]
+      end
+    end
+
+    describe '#non_range_fields' do
+      it 'returns non-range fields' do
+        expect(subject.send(:non_range_fields).keys).to eq [ "id", "sku", "name", "description", "stock", "price", "featured", "available_to_date", "available_to_time", "published_at" ]
+      end
+    end
+
+    describe '#range_fields' do
+      it 'returns range fields' do
+        expect(subject.send(:range_fields).keys).to eq []
+      end
+    end
+
+    describe '#association_fields' do
+      it 'returns association fields' do
+        expect(subject.send(:association_fields).keys).to eq [ "product_detail", "order_items", "category", "tags" ]
+      end
+    end
+
+    describe '#many_association_fields' do
+      it 'returns many-association fields' do
+        expect(subject.send(:many_association_fields).keys).to eq [ "order_items", "tags" ]
+      end
+    end
+
+    describe '#belongs_to_fields' do
+      it 'returns belongs-to fields' do
+        expect(subject.send(:belongs_to_fields).keys).to eq [ "category" ]
+      end
+    end
+  end
+
+  describe 'model that has polymorphic belongs-to' do
+    let(:model_decorator) { Wallaby::ActiveRecord::ModelDecorator.new Picture }
+    describe '#simple_field_names' do
+      it 'returns non-range fields' do
+        expect(subject.simple_field_names).to eq [ "name", "file", "imageable_id", "imageable_type" ]
+      end
+    end
+
+    describe '#compound_hashed_fields' do
+      it 'returns range fields' do
+        expect(subject.compound_hashed_fields).to eq({})
+      end
+    end
+  end
+end
