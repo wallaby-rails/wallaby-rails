3a6d5a3c416c73e1bfec91b958722aa6886d4d6f
Tian Chen <me@tian.im>
2016-04-06 17:39:14 -0400

    add validator for model_servicer

---
+++ app/adaptors/wallaby/active_record/model_servicer/validator.rb
@@ -0,0 +1,16 @@
+class Wallaby::ActiveRecord::ModelServicer::Validator
+  def initialize(model_decorator)
+    @model_decorator = model_decorator
+  end
+
+  def valid?(resource)
+    resource.attributes.each do |field_name, values|
+      next unless metadata = @model_decorator.fields[field_name]
+      if %w( daterange tsrange tstzrange ).include?(metadata[:type]) &&
+        values.try(:any?, &:blank?)
+        resource.errors.add field_name, 'required for range data'
+      end
+    end
+    resource.errors.blank?
+  end
+end
