8234696402bbec3cb74245c8017271da3145e41c
Tian Chen <me@tian.im>
2016-04-09 23:25:57 -0400

    re-style the search form and add clear cache button

--- app/assets/javascripts/wallaby/application.js
+++ app/assets/javascripts/wallaby/application.js
@@ -1,6 +1,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip({ html: true });
+jQuery(function ($) {
+  $('[data-toggle="tooltip"]').tooltip({ html: true })
+
+  $('.search-form').each(function () {
+    var $this = $(this)
+    $(this).find('input:visible').on('focus', function () {
+      $this.addClass('focus')
+    }).on('blur', function () {
+      $this.removeClass('focus')
+    })
+  })
 })
