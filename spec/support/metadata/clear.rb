6873ec6fd867a26a6dbd6c426dbe3a714d3b5429
Tian Chen <me@tian.im>
2016-04-12 13:49:40 -0400

    change to garbage_collection at the before block for specs

--- spec/support/metadata/clear.rb
+++ spec/support/metadata/clear.rb
@@ -1,7 +1,5 @@
 RSpec.configure do |config|
-  config.around do |example|
-    ObjectSpace.garbage_collect if example.metadata[:clear] == :object_space
-    example.run
+  config.before do |example|
     ObjectSpace.garbage_collect if example.metadata[:clear] == :object_space
   end
 end
