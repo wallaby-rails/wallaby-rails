5305a58f43eb411162ab613819930b11ae56ec8b
Tian Chen <me@tian.im>
2016-04-13 16:32:18 -0400

    refactor the sort helper methods

--- app/helpers/wallaby/sorting_helper.rb
+++ app/helpers/wallaby/sorting_helper.rb
@@ -1,24 +1,31 @@
 module Wallaby::SortingHelper
-  def sort_param(param_key)
-    key     = param_key.to_s
-    orders  = [ 'asc', 'desc', nil ]
-    {}.tap do |hash|
-      sortings  = Hash[ *params[:sort].to_s.split(',').map{ |v| v.split ' ' }.flatten(1) ]
-      nex_index = (orders.index(sortings[key]) + 1) % orders.length
-      if orders[nex_index].nil?
-        sortings.delete key
+  def sort_hash
+    @sort_hash ||= begin
+      array = params[:sort].to_s.split(%r(\s*,\s*)).map{ |v| v.split %r(\s+) }
+      Wallaby::Utils.to_hash array
+    end
+  end
+
+  def new_sort_param(field_name)
+    field_name  = field_name.to_s
+    orders      = [ 'asc', 'desc', nil ]
+    params.dup.tap do |hash|
+      sortings = sort_hash.dup
+      next_index = (orders.index(sortings[field_name]) + 1) % orders.length
+      if orders[next_index].nil?
+        sortings.delete field_name
       else
-        sortings[key] = orders[nex_index]
+        sortings[field_name] = orders[next_index]
       end
       if sortings.present?
         hash[:sort] = sortings.to_a.map{ |v| v.join ' ' }.join ','
+      else
+        hash.delete :sort
       end
     end
   end
 
-  def sort_class(param_key)
-    key = param_key.to_s
-    sortings = Hash[ *params[:sort].to_s.split(',').map{ |v| v.split ' ' }.flatten(1) ]
-    sortings[key]
+  def sort_class(field_name)
+    sort_hash[ field_name.to_s ]
   end
 end
--- spec/helpers/wallaby/sorting_helper.rb
+++ spec/helpers/wallaby/sorting_helper.rb
@@ -1,30 +1,76 @@
 require 'rails_helper'
 
 describe Wallaby::LinksHelper, :current_user do
-  describe '#sort_param' do
+  describe '#sort_hash' do
     it 'returns a sorting hash' do
-      expect(helper.sort_param :name).to eq sort: 'name asc'
       helper.params[:sort] = 'name asc'
-      expect(helper.sort_param :name).to eq sort: 'name desc'
+      expect(helper.sort_hash).to eq 'name' => 'asc'
+      helper.instance_variable_set '@sort_hash', nil
+
+      helper.params[:sort] = 'name asc,updated_at desc'
+      expect(helper.sort_hash).to eq 'name' => 'asc', 'updated_at' => 'desc'
+      helper.instance_variable_set '@sort_hash', nil
+
+      helper.params[:sort] = 'name  asc , updated_at  desc'
+      expect(helper.sort_hash).to eq 'name' => 'asc', 'updated_at' => 'desc'
+      helper.instance_variable_set '@sort_hash', nil
+    end
+  end
+
+  describe '#new_sort_param' do
+    it 'does not change either params or sort_hash' do
+      expect(helper.sort_hash).to eq({})
+      expect(helper.new_sort_param :name).to eq sort: 'name asc'
+      expect(helper.sort_hash).to eq({})
+      expect(helper.params).to eq({})
+    end
+
+    it 'includes other params' do
+      helper.params[:q] = 'search keyword'
+      expect(helper.new_sort_param :name).to eq q: 'search keyword', sort: 'name asc'
+    end
+
+    it 'returns a sorting hash' do
+      expect(helper.params).to eq({})
+      expect(helper.new_sort_param :name).to eq sort: 'name asc'
+      helper.instance_variable_set '@sort_hash', nil
+
+      helper.params[:sort] = 'name asc'
+      expect(helper.new_sort_param :name).to eq sort: 'name desc'
+      helper.instance_variable_set '@sort_hash', nil
+
       helper.params[:sort] = 'name desc'
-      expect(helper.sort_param :name).to eq Hash.new
-      expect(helper.sort_param :updated_at).to eq sort: 'name desc,updated_at asc'
+      expect(helper.new_sort_param :name).to eq({})
+      helper.instance_variable_set '@sort_hash', nil
+
       helper.params[:sort] = 'name desc,updated_at asc'
-      expect(helper.sort_param :updated_at).to eq sort: 'name desc,updated_at desc'
+      expect(helper.new_sort_param :name).to eq sort: 'updated_at asc'
+      expect(helper.new_sort_param :updated_at).to eq sort: 'name desc,updated_at desc'
+      helper.instance_variable_set '@sort_hash', nil
+
       helper.params[:sort] = 'name desc,updated_at desc'
-      expect(helper.sort_param :updated_at).to eq sort: 'name desc'
+      expect(helper.new_sort_param :name).to eq sort: 'updated_at desc'
+      expect(helper.new_sort_param :updated_at).to eq sort: 'name desc'
     end
   end
 
   describe '#sort_class' do
     it 'returns a sorting class' do
       expect(helper.sort_class :name).to be_nil
+      helper.instance_variable_set '@sort_hash', nil
+
       helper.params[:sort] = 'name asc'
       expect(helper.sort_class :name).to eq 'asc'
+      helper.instance_variable_set '@sort_hash', nil
+
       helper.params[:sort] = 'name desc'
       expect(helper.sort_class :name).to eq 'desc'
+      helper.instance_variable_set '@sort_hash', nil
+
       helper.params[:sort] = 'name desc,updated_at asc'
       expect(helper.sort_class :updated_at).to eq 'asc'
+      helper.instance_variable_set '@sort_hash', nil
+
       helper.params[:sort] = 'name desc,updated_at desc'
       expect(helper.sort_class :updated_at).to eq 'desc'
     end
