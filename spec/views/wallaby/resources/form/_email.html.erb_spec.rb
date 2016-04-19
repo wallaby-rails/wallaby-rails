08c5cc4991fa861281e3e0f161fdf41beea64387
Tian Chen <me@tian.im>
2016-04-19 13:56:32 -0400

    email type for form/index/show

---
+++ spec/views/wallaby/resources/form/_email.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+partial_name = 'form/email'
+describe partial_name do
+  let(:partial)   { "wallaby/resources/#{ partial_name }.html.erb" }
+  let(:form)        { Wallaby::FormBuilder.new object.model_name.param_key, object, view, { } }
+  let(:object)      { AllPostgresType.new field_name => value }
+  let(:field_name)  { :string }
+  let(:value)       { 'tian@reinteractive.com' }
+  let(:metadata)    { Hash.new }
+
+  before { render partial, form: form, object: object, field_name: field_name, value: value, metadata: metadata }
+
+  it 'renders the email form' do
+    expect(rendered).to eq "<div class=\"form-group \">\n  <label for=\"all_postgres_type_string\">String</label>\n  <input class=\"form-control\" type=\"email\" value=\"tian@reinteractive.com\" name=\"all_postgres_type[string]\" id=\"all_postgres_type_string\" />\n  \n</div>\n"
+  end
+
+  context 'when value is nil' do
+    let(:value) { nil }
+    it 'renders empty input' do
+      expect(rendered).to eq "<div class=\"form-group \">\n  <label for=\"all_postgres_type_string\">String</label>\n  <input class=\"form-control\" type=\"email\" name=\"all_postgres_type[string]\" id=\"all_postgres_type_string\" />\n  \n</div>\n"
+    end
+  end
+end
---
+++ spec/views/wallaby/resources/index/_email.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+partial_name = 'index/email'
+describe partial_name do
+  let(:partial)   { "wallaby/resources/#{ partial_name }.html.erb" }
+  let(:value)     { 'tian@reinteractive.net' }
+  let(:metadata)  { Hash.new }
+
+  before { render partial, value: value, metadata: metadata }
+
+  it 'renders the email' do
+    expect(rendered).to eq "<a href=\"mailto:tian@reinteractive.net\">tian@reinteractive.net</a>\n"
+  end
+
+  context 'when value is nil' do
+    let(:value) { nil }
+    it 'renders null' do
+      expect(rendered).to eq "<i class=\"text-muted\">&lt;null&gt;</i>\n"
+    end
+  end
+end
