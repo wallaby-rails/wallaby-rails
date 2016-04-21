94d1a8594ee1f4c314fc59b71a36c8943d97af61
Tian Chen <me@tian.im>
2016-04-20 23:34:13 -0400

    require jquery-minicolors-rails and add deflater

--- lib/wallaby/engine.rb
+++ lib/wallaby/engine.rb
@@ -6,21 +6,26 @@ require 'kaminari'
 require 'redcarpet'
 
 require 'sass-rails'
-require 'jquery-rails'
 require 'bootstrap-sass'
-require 'momentjs-rails'
 require 'bootstrap3-datetimepicker-rails'
+require 'codemirror-rails'
+require 'jquery-minicolors-rails'
+require 'jquery-rails'
+require 'momentjs-rails'
 require 'rails-bootstrap-markdown'
 require 'summernote-rails'
-require 'codemirror-rails'
 
 module Wallaby
   class Engine < ::Rails::Engine
+    # isolate_namespace Wallaby
+    initializer 'wallaby.deflate' do |app|
+      app.config.middleware.use Rack::Deflater
+    end
+
     initializer 'wallaby.initialize_cache' do |app|
       Rails.cache.delete_matched %r(\Awallaby/)
     end
 
-    # isolate_namespace Wallaby
     initializer 'wallaby.assets.precompile' do |app|
       app.config.assets.precompile += %w( wallaby/form.js wallaby/form.css )
       app.config.assets.precompile += %w( codemirror* codemirror/**/* )
