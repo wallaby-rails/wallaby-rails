bb4108a59d0f183657e6330967ef63efab345b0d
Tian Chen <me@tian.im>
2016-05-09 23:49:59 -0400

    prefix views/erb cache

--- lib/handlers/wallaby/cached_compiled_erb.rb
+++ lib/handlers/wallaby/cached_compiled_erb.rb
@@ -3,7 +3,7 @@ require 'erubis'
 # TODO: move this kind of logic into a gem called faster rails :)
 class Wallaby::CachedCompiledErb < ActionView::Template::Handlers::ERB
   def call(template)
-    Rails.cache.fetch "wallaby/#{ template.inspect }" do
+    Rails.cache.fetch "wallaby/views/erb/#{ template.inspect }" do
       super
     end
   end
