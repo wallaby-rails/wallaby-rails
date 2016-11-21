841e4e06def818e522025866cfee3887668de415
Tian Chen <me@tian.im>
2016-11-21 11:59:56 +0800

    update mysql_types_spec

--- spec/active_record/mysql_types_spec.rb
+++ spec/active_record/mysql_types_spec.rb
@@ -4,8 +4,8 @@ describe 'Mysql Types' do
   it 'returns the expected native types' do
     native_types = ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter::NATIVE_DATABASE_TYPES.keys.map &:to_s
 
-    expect(native_types.length).to eq 11
-    expect(native_types.sort).to eq [ "binary", "boolean", "date", "datetime", "decimal", "float", "integer", "primary_key", "string", "text", "time" ]
+    expect(native_types.length).to eq 12
+    expect(native_types.sort).to eq [ "binary", "boolean", "date", "datetime", "decimal", "float", "integer", "json", "primary_key", "string", "text", "time" ]
   end
 
   it 'supports the following types' do
@@ -15,7 +15,7 @@ describe 'Mysql Types' do
       end.compact.uniq
     end
 
-    expect(supporting_types.length).to eq 30
-    expect(supporting_types.sort).to eq [ "bigint", "binary", "bit", "blob", "boolean", "char", "clob", "date", "datetime", "decimal", "double", "enum", "float", "int", "longblob", "longtext", "mediumblob", "mediumint", "mediumtext", "number", "numeric", "set", "smallint", "text", "time", "timestamp", "tinyblob", "tinyint", "tinytext", "year" ]
+    expect(supporting_types.length).to eq 31
+    expect(supporting_types.sort).to eq [ "bigint", "binary", "bit", "blob", "boolean", "char", "clob", "date", "datetime", "decimal", "double", "enum", "float", "int", "json", "longblob", "longtext", "mediumblob", "mediumint", "mediumtext", "number", "numeric", "set", "smallint", "text", "time", "timestamp", "tinyblob", "tinyint", "tinytext", "year" ]
   end
 end
