2eb80d44d736ca81439aa4f5c6cdc0a5fa397787
Tian Chen <me@tian.im>
2016-11-24 18:20:53 +0800

    support ApplicationRecord

--- lib/adaptors/wallaby/active_record/model_finder.rb
+++ lib/adaptors/wallaby/active_record/model_finder.rb
@@ -1,7 +1,17 @@
 class Wallaby::ActiveRecord::ModelFinder < Wallaby::ModelFinder
+  def base
+    Rails.cache.fetch 'wallaby/active_record/model_finder/base' do
+      if defined? ::ApplicationRecord
+        ::ApplicationRecord
+      else
+        ::ActiveRecord::Base
+      end
+    end
+  end
+
   def all
     Rails.cache.fetch 'wallaby/active_record/model_finder' do
-      ActiveRecord::Base.subclasses.reject do |model_class|
+      base.subclasses.reject do |model_class|
         model_class.abstract_class? ||
         model_class.to_s.start_with?('#<') ||
         model_class.name == 'ActiveRecord::SchemaMigration' ||
