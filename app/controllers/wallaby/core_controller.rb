580ed7b42b5a096ab33679f8c07bf492505e9240
Tian Chen <me@tian.im>
2017-04-07 13:54:18 +1000

    rubocop is now happy with core_controller

--- app/controllers/wallaby/core_controller.rb
+++ app/controllers/wallaby/core_controller.rb
@@ -1,14 +1,16 @@
 module Wallaby
+  # Core controller to provide basic helper methods for sub classes
   class CoreController < SecureController
     helper Wallaby::CoreHelper
 
-    before_action :authenticate_user!, except: [ :status ]
+    before_action :authenticate_user!, except: [:status]
 
     def status
       render plain: 'healthy'
     end
 
     protected
+
     begin # helper methods
       helper_method \
         :current_resources_name,
@@ -19,9 +21,9 @@ module Wallaby
       end
 
       def current_model_class
-        @current_model_class ||= if self.class.respond_to? :model_class
-          self.class.model_class
-        end || Wallaby::Utils.to_model_class(current_resources_name)
+        @current_model_class ||=
+          self.class.respond_to?(:model_class) && self.class.model_class \
+          || Wallaby::Utils.to_model_class(current_resources_name)
       end
     end
   end
