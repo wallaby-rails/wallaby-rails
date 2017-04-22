4c8c69096b1f8898d8aafccefdb4ccb1de4ef726
Tian Chen <me@tian.im>
2017-04-22 23:32:13 +1000

    rewrite map to use mappers

--- spec/services/wallaby/map_spec.rb
+++ spec/services/wallaby/map_spec.rb
@@ -2,50 +2,165 @@ require 'rails_helper'
 
 describe Wallaby::Map do
   before do
-    allow_any_instance_of(Wallaby::ActiveRecord::ModelFinder).to receive(:all) { [ AllPostgresType, AllMysqlType, AllSqliteType ] }
+    allow_any_instance_of(Wallaby::ActiveRecord::ModelFinder).to receive(:all) {
+      [AllPostgresType, AllMysqlType, AllSqliteType]
+    }
   end
 
   describe '.mode_map' do
     before do
-      expect(Wallaby::Mode).to receive(:subclasses) { Array Wallaby::ActiveRecord }
+      class SuperMode
+        def self.model_finder; Finder; end
+        class Finder
+          def all; [AllPostgresType, AllMysqlType, AllSqliteType]; end
+        end
+      end
+      expect(Wallaby::Mode).to receive(:subclasses) { [SuperMode] }
     end
-    it 'returns a map for model -> mode' do
-      expect(described_class.mode_map).to eq AllPostgresType => Wallaby::ActiveRecord, AllMysqlType => Wallaby::ActiveRecord, AllSqliteType => Wallaby::ActiveRecord
+
+    it 'returns a map of model -> mode' do
+      expect(described_class.mode_map).to eq \
+        AllPostgresType => SuperMode,
+        AllMysqlType => SuperMode,
+        AllSqliteType => SuperMode
+    end
+
+    it 'memorizes the map' do
+      expect(described_class::ModeMapper) \
+        .to receive(:new).once.and_return(double(map: []))
+      described_class.mode_map
+      described_class.mode_map
+      described_class.mode_map
+    end
+  end
+
+  describe '.controller_map' do
+    before do
+      class FashionController1
+        def self.model_class; AllPostgresType; end
+      end
+      class FashionController2
+        def self.model_class; AllMysqlType; end
+      end
+      expect(Wallaby::ResourcesController).to receive(:subclasses) {
+        [FashionController1, FashionController2]
+      }
+    end
+
+    it 'returns a map of model -> controller' do
+      expect(described_class.controller_map).to eq \
+        AllPostgresType => FashionController1,
+        AllMysqlType => FashionController2
+    end
+
+    it 'memorizes the map' do
+      described_class.controller_map
+      described_class.controller_map
+      described_class.controller_map
+    end
+  end
+
+  describe '.decorator_map' do
+    before do
+      class FashionDecorator1
+        def self.model_class; AllPostgresType; end
+      end
+      class FashionDecorator2
+        def self.model_class; AllMysqlType; end
+      end
+      expect(Wallaby::ResourceDecorator).to receive(:subclasses) {
+        [FashionDecorator1, FashionDecorator2]
+      }
+    end
+
+    it 'returns a map of model -> decorator' do
+      expect(described_class.decorator_map).to eq \
+        AllPostgresType => FashionDecorator1,
+        AllMysqlType => FashionDecorator2
+    end
+
+    it 'memorizes the map' do
+      described_class.decorator_map
+      described_class.decorator_map
+      described_class.decorator_map
+    end
+  end
+
+  describe '.servicer_map' do
+    before do
+      class FashionServicer1
+        def self.model_class; AllPostgresType; end
+      end
+      class FashionServicer2
+        def self.model_class; AllMysqlType; end
+      end
+      expect(Wallaby::ModelServicer).to receive(:subclasses) {
+        [FashionServicer1, FashionServicer2]
+      }
+    end
+
+    it 'returns a map of model -> servicer' do
+      expect(described_class.servicer_map).to eq \
+        AllPostgresType => FashionServicer1,
+        AllMysqlType => FashionServicer2
+    end
+
+    it 'memorizes the map' do
+      described_class.servicer_map
+      described_class.servicer_map
+      described_class.servicer_map
     end
   end
 
   describe '.model_classes' do
+    let(:configuration) { Wallaby::Configuration.new }
+
     before do
-      expect(Wallaby::Mode).to receive(:subclasses) { Array Wallaby::ActiveRecord }
-    end
-    it 'returns model classes' do
-      expect(described_class.model_classes).to eq [ AllPostgresType, AllMysqlType, AllSqliteType ]
+      expect(Wallaby::Mode).to receive(:subclasses) { [SuperMode] }
+      expect(Wallaby).to receive(:configuration) { configuration }
     end
 
-    describe 'model filter' do
-      let(:configuration) { Wallaby::Configuration.new }
+    it 'returns all models' do
+      expect(described_class.model_classes).to eq \
+        [AllPostgresType, AllMysqlType, AllSqliteType]
+    end
 
-      context 'when models is set' do
-        it 'returns models' do
-          configuration.models = [ AllPostgresType, AllMysqlType ]
-          expect(described_class.model_classes configuration).to eq [ AllPostgresType, AllMysqlType ]
+    context 'there are excludes' do
+      before do
+        configuration.models.exclude AllPostgresType
+      end
+
+      it 'excludes AllPostgresType' do
+        expect(described_class.model_classes).to \
+          eq [AllMysqlType, AllSqliteType]
+      end
+
+      context 'models are set' do
+        before do
+          configuration.models.set AllSqliteType
         end
 
-        context 'when models has invalid model' do
-          it 'raises invalid error' do
-            stub_const 'InvalidModel', Class.new
-            configuration.models = [ InvalidModel ]
-            expect { described_class.model_classes configuration }.to raise_error Wallaby::InvalidError, "InvalidModel are invalid models."
+        it 'returns the models being set' do
+          expect(described_class.model_classes).to eq [AllSqliteType]
+        end
+
+        context 'some of the models being set are invalid' do
+          before do
+            configuration.models.set SuperMode, AllSqliteType
+          end
+
+          it 'raises error' do
+            expect { described_class.model_classes }.to \
+              raise_error Wallaby::InvalidError
           end
         end
       end
+    end
 
-      context 'when exclude is set' do
-        it 'returns models without excludes' do
-          configuration.models.exclude AllPostgresType, AllSqliteType
-          expect(described_class.model_classes configuration).to eq [ AllMysqlType ]
-        end
-      end
+    it 'memorizes the map' do
+      described_class.model_classes
+      described_class.model_classes
+      described_class.model_classes
     end
   end
 end
