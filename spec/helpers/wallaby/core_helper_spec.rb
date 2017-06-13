b3093c2338104db2fad6ee6e87c4e11aaa98e0cd
Tian Chen <me@tian.im>
2017-06-13 22:04:02 +1000

    remove page_title and current_model_label

--- spec/helpers/wallaby/core_helper_spec.rb
+++ spec/helpers/wallaby/core_helper_spec.rb
@@ -2,39 +2,35 @@ require 'rails_helper'
 
 describe Wallaby::CoreHelper do
   describe '#body_class' do
+    before do
+      # @see Wallaby::CoreController#current_resources_name
+      def helper.current_resources_name
+        params[:resources]
+      end
+    end
+
+    it 'returns body class' do
+      helper.params[:action] = 'index'
+      expect(helper.body_class).to eq 'index'
+    end
+
     context 'when resources name is blank' do
       it 'returns body class' do
-        allow(helper).to receive :current_resources_name
         expect(helper.body_class).to eq ''
       end
     end
 
     context 'when resources name is present' do
       it 'returns body class' do
-        allow(helper).to receive(:current_resources_name) { 'wallaby::posts' }
+        helper.params[:resources] = 'wallaby::posts'
         expect(helper.body_class).to eq 'wallaby__posts'
       end
     end
 
     context 'when custom_body_class is present' do
       it 'returns body class' do
-        allow(helper).to receive :current_resources_name
         helper.content_for :custom_body_class, 'body'
         expect(helper.body_class).to eq 'body'
-      end
-    end
-  end
-
-  describe '#current_model_label' do
-    it 'returns current model label' do
-      allow(helper).to receive :current_resources_name
-      expect(helper.current_model_label).to eq 'Resources'
-    end
-
-    context 'when resources_name is given' do
-      it 'returns current model label' do
-        allow(helper).to receive(:current_resources_name) { 'posts' }
-        expect(helper.current_model_label).to eq 'Resource: Post'
       end
     end
   end
