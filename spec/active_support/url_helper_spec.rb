4b534a928f25e91d4906c74b5e2317f2918594bf
Tian Chen <me@tian.im>
2017-06-23 17:46:46 +1000

    add tests for rails hash and parameters so that we know what has changed

---
+++ spec/active_support/url_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'URL helpers', type: :helper do
+  describe 'action_view/routing_url_for' do
+    it 'returns url' do
+      super_method = helper.method(:url_for).super_method
+      expect(super_method.call(parameters(controller: 'application', action: 'index').permit!)).to eq '/test/purpose'
+    end
+  end
+
+  describe 'wallaby_engine.resources_path helper' do
+    it 'returns url' do
+      expect(helper.wallaby_engine.resources_path(action: 'index', resources: 'products')).to eq '/admin/products'
+      expect(helper.wallaby_engine.resources_path(parameters(action: 'index', resources: 'products').permit!)).to match '/admin/%23%3CActionController::Parameters'
+    end
+  end
+end
