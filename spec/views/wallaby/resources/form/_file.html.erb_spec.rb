77553c4c430251f91f3e97391a2ce20d7a38ffcc
Tian Chen <me@tian.im>
2017-11-24 00:07:40 +1100

    fix spec

--- spec/views/wallaby/resources/form/_file.html.erb_spec.rb
+++ spec/views/wallaby/resources/form/_file.html.erb_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-field_name = __FILE__[/_(.+)\.html\.erb_spec\.rb$/, 1]
+field_name = 'blob'
 type = __FILE__[%r{/([^/]+)/_}, 1]
 describe field_name do
   it_behaves_like \
     "#{type} partial", field_name,
     value: '001111000001',
+    partial: 'file',
     type: 'file',
     model_class: AllMysqlType,
     input_selector: 'input.hidden',
