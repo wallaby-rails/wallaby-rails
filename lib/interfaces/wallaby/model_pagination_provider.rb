3b4425bd906ad571e549a9f931d028a796dba2ae
Tian Chen <me@tian.im>
2017-12-14 00:54:25 +1100

    add documentation to interfaces

--- lib/interfaces/wallaby/model_pagination_provider.rb
+++ lib/interfaces/wallaby/model_pagination_provider.rb
@@ -1,6 +1,10 @@
 module Wallaby
   # Model pagination provider interface
   class ModelPaginationProvider
+    # @param collection
+    # @param params [ActionController::Parameters]
+    # @param options [Hash] options
+    # @param model_decorator [Wallaby::ModelDecorator, nil]
     def initialize(collection, params, options: {}, model_decorator: nil)
       @collection = collection
       @params = params
@@ -8,62 +12,91 @@ module Wallaby
       @model_decorator = model_decorator
     end
 
+    # If a collection has pagination feature
     def paginatable?
       raise NotImplemented
     end
 
+    # Check and see if it's the first page
+    # @return boolean
     def first_page?
       page_number > first_page_number
     end
 
+    # Check and see if it's the previous page
+    # @return boolean
     def prev_page?
       page_number > first_page_number
     end
 
+    # Check and see if it's the last page
+    # @return boolean
     def last_page?
       page_number < last_page_number
     end
 
+    # Check and see if it's the next page
+    # @return boolean
     def next_page?
       page_number < last_page_number
     end
 
+    # Find out the offset `from`
+    # @return [Integer]
     def from
       (page_number - 1) * page_size + 1
     end
 
+    # Find out the offset `to`
+    # @return [Integer]
     def to
       [page_number * page_size, total].min
     end
 
+    # Find out the total count of current query
+    # @return [Integer]
     def total
       raise NotImplemented
     end
 
+    # Find out the current page size
+    # @return [Integer]
     def page_size
       raise NotImplemented
     end
 
+    # Find out the current page number
+    # @return [Integer]
     def page_number
       raise NotImplemented
     end
 
+    # Page number of first page
+    # @return [Integer]
     def first_page_number
       1
     end
 
+    # Page number of last page
+    # @return [Integer]
     def last_page_number
       number_of_pages
     end
 
+    # Page number of previous page
+    # @return [Integer]
     def prev_page_number
       [page_number - 1, first_page_number].max
     end
 
+    # Page number of next page
+    # @return [Integer]
     def next_page_number
       [page_number + 1, last_page_number].min
     end
 
+    # Total number of pages
+    # @return [Integer]
     def number_of_pages
       (total / page_size.to_f).ceil
     end
