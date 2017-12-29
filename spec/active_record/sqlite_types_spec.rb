983778cb961d9a030a891ab77ab005fe7c6b7ba4
Tian Chen <me@tian.im>
2017-12-29 23:31:40 +1100

    fix coverage

--- spec/active_record/sqlite_types_spec.rb
+++ spec/active_record/sqlite_types_spec.rb
@@ -1,41 +1,44 @@
 require 'rails_helper'
 
 describe 'Sqlite Types' do
-  if Rails::VERSION::MAJOR == 5 && Rails::VERSION::MINOR == 2
-    it 'returns the expected native types' do
-      native_types = ActiveRecord::ConnectionAdapters::SQLite3Adapter::NATIVE_DATABASE_TYPES.keys.map(&:to_s)
+  let(:version_expected) do
+    {
+      5 => {
+        2 => {
+          size_of_native_types: 12,
+          native_types: %w(binary boolean date datetime decimal float integer json primary_key string text time),
+          size_of_supporting_types: 17,
+          supporting_types: %w(binary blob boolean char clob date datetime decimal double float int json number numeric text time timestamp)
+        }
+      }
+    }
+  end
+  let(:general) do
+    {
+      size_of_native_types: 11,
+      native_types: %w(binary boolean date datetime decimal float integer primary_key string text time),
+      size_of_supporting_types: 16,
+      supporting_types: %w(binary blob boolean char clob date datetime decimal double float int number numeric text time timestamp)
+    }
+  end
 
-      expect(native_types.length).to eq 12
-      expect(native_types.sort).to eq %w(binary boolean date datetime decimal float integer json primary_key string text time)
+  let(:expected) { minor version_expected, general }
+
+  it 'returns the expected native types' do
+    native_types = ActiveRecord::ConnectionAdapters::SQLite3Adapter::NATIVE_DATABASE_TYPES.keys.map(&:to_s)
+
+    expect(native_types.length).to eq expected[:size_of_native_types]
+    expect(native_types.sort).to eq expected[:native_types]
+  end
+
+  it 'supports the following types' do
+    supporting_types = AllSqliteType.connection.send(:type_map).try do |type_map|
+      type_map.instance_variable_get('@mapping').keys.map do |key|
+        key.source.gsub(/\^|(\\.*\Z)/, '')
+      end.compact.uniq
     end
 
-    it 'supports the following types' do
-      supporting_types = AllSqliteType.connection.send(:type_map).try do |type_map|
-        type_map.instance_variable_get('@mapping').keys.map do |key|
-          key.source.gsub(/\^|(\\.*\Z)/, '')
-        end.compact.uniq
-      end
-
-      expect(supporting_types.length).to eq 17
-      expect(supporting_types.sort).to eq %w(binary blob boolean char clob date datetime decimal double float int json number numeric text time timestamp)
-    end
-  else
-    it 'returns the expected native types' do
-      native_types = ActiveRecord::ConnectionAdapters::SQLite3Adapter::NATIVE_DATABASE_TYPES.keys.map(&:to_s)
-
-      expect(native_types.length).to eq 11
-      expect(native_types.sort).to eq %w(binary boolean date datetime decimal float integer primary_key string text time)
-    end
-
-    it 'supports the following types' do
-      supporting_types = AllSqliteType.connection.send(:type_map).try do |type_map|
-        type_map.instance_variable_get('@mapping').keys.map do |key|
-          key.source.gsub(/\^|(\\.*\Z)/, '')
-        end.compact.uniq
-      end
-
-      expect(supporting_types.length).to eq 16
-      expect(supporting_types.sort).to eq %w(binary blob boolean char clob date datetime decimal double float int number numeric text time timestamp)
-    end
+    expect(supporting_types.length).to eq expected[:size_of_supporting_types]
+    expect(supporting_types.sort).to eq expected[:supporting_types]
   end
 end
