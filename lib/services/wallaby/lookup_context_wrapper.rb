0f73904fd89498758ee7e9c9dd35f813c5dd456a
Tian Chen <me@tian.im>
2018-01-01 20:39:20 +1100

    update lookup_wrapper

--- lib/services/wallaby/lookup_context_wrapper.rb
+++ lib/services/wallaby/lookup_context_wrapper.rb
@@ -1,8 +1,11 @@
 module Wallaby
-  # Lookup context wrapper
+  # Lookup context wrapper.
+  # This is to take care of missing template in production to return string
+  # template.
+  # @see Wallaby::PartialRenderer
   class LookupContextWrapper
     origin_methods = ::ActionView::LookupContext.instance_methods \
-        - ::Object.instance_methods - %i(find_template)
+      - ::Object.instance_methods - %i(find_template)
     delegate(*origin_methods, to: :@lookup_context)
 
     def initialize(lookup_context)
@@ -10,28 +13,13 @@ module Wallaby
     end
 
     def find_template(*args)
-      key = args.join SLASH
-      caching key do
-        @lookup_context.find_template(*args)
-      end
+      @lookup_context.find_template(*args)
+    rescue ::ActionView::MissingTemplate
+      raise if Rails.env.development?
+      BlankTemplate.new
     end
 
-    protected
-
-    def caching(key)
-      @templates ||= {}
-      unless @templates.key? key
-        @templates[key] = begin
-          yield
-        rescue ::ActionView::MissingTemplate
-          raise if Rails.env.development?
-          BlankTemplate.new
-        end
-      end
-      @templates[key]
-    end
-
-    # Blank template
+    # Blank template to return nil. It should be used by production
     class BlankTemplate < ::ActionView::Template::HTML
       def initialize
         super nil
