1409989bd4e9d183200d875cbb5068dd071dd47d
Tian Chen <me@tian.im>
2018-01-08 20:42:31 +1100

    fix specs

--- spec/services/wallaby/sorting/link_builder_spec.rb
+++ spec/services/wallaby/sorting/link_builder_spec.rb
@@ -8,45 +8,45 @@ describe Wallaby::Sorting::LinkBuilder, :current_user, type: :helper do
   describe '#build' do
     it 'returns a sort link when field is origin and not associations' do
       expect(subject.build('tags')).to eq 'Tags'
-      expect(subject.build('id')).to eq '<a href="/admin/products?sort=id+asc">Id</a>'
+      expect(subject.build('id')).to eq '<a title="Product" href="/admin/products?sort=id+asc">Id</a>'
     end
 
     it 'returns a sort link even when field is not origin but sort_field_name is set' do
       model_decorator.index_fields['model'] = { label: 'Model', is_origin: false, is_association: false, sort_field_name: 'id' }
 
-      expect(subject.build('model')).to eq '<a href="/admin/products?sort=id+asc">Model</a>'
+      expect(subject.build('model')).to eq '<a title="Product" href="/admin/products?sort=id+asc">Model</a>'
     end
 
     it 'includes other params' do
       params[:q] = 'search keyword'
-      expect(subject.build(:name)).to eq '<a href="/admin/products?q=search+keyword&amp;sort=name+asc">Name</a>'
+      expect(subject.build(:name)).to eq '<a title="Product" href="/admin/products?q=search+keyword&amp;sort=name+asc">Name</a>'
     end
 
     it 'returns a sorting hash' do
-      expect(subject.build(:name)).to eq '<a href="/admin/products?sort=name+asc">Name</a>'
+      expect(subject.build(:name)).to eq '<a title="Product" href="/admin/products?sort=name+asc">Name</a>'
       subject.instance_variable_set '@current_sort', nil
       subject.instance_variable_set '@next_builder', nil
 
       params[:sort] = 'name asc'
-      expect(subject.build(:name)).to eq '<a href="/admin/products?sort=name+desc">Name</a>'
+      expect(subject.build(:name)).to eq '<a title="Product" href="/admin/products?sort=name+desc">Name</a>'
       subject.instance_variable_set '@current_sort', nil
       subject.instance_variable_set '@next_builder', nil
 
       params[:sort] = 'name desc'
-      expect(subject.build(:name)).to eq '<a href="/admin/products">Name</a>'
+      expect(subject.build(:name)).to eq '<a title="Product" href="/admin/products">Name</a>'
       subject.instance_variable_set '@current_sort', nil
       subject.instance_variable_set '@next_builder', nil
 
       model_decorator.index_fields[:updated_at] = { sort_field_name: 'updated_at' }
       params[:sort] = 'name desc,updated_at asc'
-      expect(subject.build(:name)).to eq '<a href="/admin/products?sort=updated_at+asc">Name</a>'
-      expect(subject.build(:updated_at)).to eq '<a href="/admin/products?sort=name+desc%2Cupdated_at+desc">Updated at</a>'
+      expect(subject.build(:name)).to eq '<a title="Product" href="/admin/products?sort=updated_at+asc">Name</a>'
+      expect(subject.build(:updated_at)).to eq '<a title="Product" href="/admin/products?sort=name+desc%2Cupdated_at+desc">Updated at</a>'
       subject.instance_variable_set '@current_sort', nil
       subject.instance_variable_set '@next_builder', nil
 
       params[:sort] = 'name desc,updated_at desc'
-      expect(subject.build(:name)).to eq '<a href="/admin/products?sort=updated_at+desc">Name</a>'
-      expect(subject.build(:updated_at)).to eq '<a href="/admin/products?sort=name+desc">Updated at</a>'
+      expect(subject.build(:name)).to eq '<a title="Product" href="/admin/products?sort=updated_at+desc">Name</a>'
+      expect(subject.build(:updated_at)).to eq '<a title="Product" href="/admin/products?sort=name+desc">Updated at</a>'
     end
   end
 
