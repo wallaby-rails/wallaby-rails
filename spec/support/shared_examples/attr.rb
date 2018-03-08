04675da7e0a4f98e2c1d5ea8f75c75a828512050
Tian Chen <me@tian.im>
2018-03-09 09:24:48 +1100

    add mapping to configuration

---
+++ spec/support/shared_examples/attr.rb
@@ -0,0 +1,18 @@
+RSpec.shared_examples 'has attribute with default value' do |attr_name, default_value, valid_value = 'default'|
+  it 'modifies the attribute' do
+    expect(subject.send("#{attr_name}=", valid_value)).to valid_value
+    expect(subject.send(attr_name)).to eq valid_value
+  end
+
+  it 'returns default_value' do
+    expect(subject.send(attr_name)).to eq default_value
+  end
+end
+
+RSpec.shared_examples 'has accessible attribute' do |attr_name, valid_value = 'default_value'|
+  it 'accesses the attribute' do
+    expect(subject.send(attr_name)).to be_nil
+    expect(subject.send("#{attr_name}=", valid_value)).to eq valid_value
+    expect(subject.send(attr_name)).to eq valid_value
+  end
+end
