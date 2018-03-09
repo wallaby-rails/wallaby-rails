34f17a2cad26239b08ce614bed3b962f8136d55a
Tian Chen <me@tian.im>
2018-03-09 18:08:18 +1100

    use shared_examples

--- spec/lib/wallaby/configuration/metadata_spec.rb
+++ spec/lib/wallaby/configuration/metadata_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 describe Wallaby::Configuration::Metadata do
-  describe '#max' do
-    it 'returns max' do
-      expect(subject.max).to eq Wallaby::DEFAULT_MAX
-      subject.max = 50
-      expect(subject.max).to eq 50
-    end
-  end
+  it_behaves_like \
+    'has attribute with default value',
+    :max, Wallaby::DEFAULT_MAX
 end
