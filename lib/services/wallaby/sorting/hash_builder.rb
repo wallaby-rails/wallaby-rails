ee22ccbdfb20e7dda451d9a2d58edd193dd6de69
Tian Chen <me@tian.im>
2018-09-12 07:15:22 +0800

    update docs

--- lib/services/wallaby/sorting/hash_builder.rb
+++ lib/services/wallaby/sorting/hash_builder.rb
@@ -9,9 +9,7 @@ module Wallaby
       # @return [Hash] { field_name => 'asc|desc' }
       def self.build(sort_string)
         ::ActiveSupport::HashWithIndifferentAccess.new.tap do |hash|
-          (sort_string || EMPTY_STRING).scan SORT_REGEX do |_, key, order|
-            hash[key] = order
-          end
+          (sort_string || EMPTY_STRING).scan SORT_REGEX { |_, key, order| hash[key] = order }
         end
       end
     end
