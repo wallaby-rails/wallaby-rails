6cec638a68980a80c204f9701c54c3672bc97fac
Tian Chen <me@tian.im>
2019-02-13 13:54:45 +0800

    update specs

--- spec/renderers/wallaby/resources/index/tinyblob_html_spec.rb
+++ spec/renderers/wallaby/resources/index/tinyblob_html_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
-if Rails::VERSION::MAJOR >= 5
-  field_name = field_name_from __FILE__
-  type = type_from __FILE__
-  klass = cell_class_from __FILE__
-  describe klass, type: :view do
-    it_behaves_like \
-      "#{type} cell", field_name,
-      value: '010111',
-      model_class: AllMysqlType,
-      skip_general: true do
+field_name = Rails::VERSION::MAJOR >= 5 ? field_name_from(__FILE__) : 'string'
+type = type_from __FILE__
+klass = cell_class_from __FILE__
+describe klass, type: :view do
+  it_behaves_like \
+    "#{type} cell", field_name,
+    value: '010111',
+    model_class: AllMysqlType,
+    skip_general: true do
 
-      it 'renders the tinyblob' do
-        expect(rendered).to include view.muted('tinyblob')
-      end
+    it 'renders the tinyblob' do
+      expect(rendered).to include view.muted('tinyblob')
     end
   end
 end
