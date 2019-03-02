3937113f64efb07af65360fe2211a6860ee7cdf4
Tian Chen <me@tian.im>
2019-03-02 14:41:42 +1100

    change back to constantize

--- lib/utils/wallaby/preload_utils.rb
+++ lib/utils/wallaby/preload_utils.rb
@@ -11,19 +11,7 @@ module Wallaby
       # @params load_path [String, Pathname]
       # @see {https://api.rubyonrails.org/classes/Rails/Engine.html#method-i-eager_load-21 Rails::Engine#eager_load!}
       def require_one(load_path)
-        Dir.glob("#{load_path}/**/*.rb").sort.each do |file_path|
-          # `constantize` is used to make Rails to handle all sort of load errors
-          # NOTE: don't try to use `ActiveSupport::Dependencies::Loadable.require_dependency`
-          # as `require_dependency` does not take care all errors when class/module is declared
-          begin
-            module_name = file_path[%r{app/[^/]+/(.+)\.rb}, 1].gsub('/concerns/', '/')
-            class_name = module_name.camelize
-            class_name.constantize unless Module.const_defined? class_name
-          rescue NameError, LoadError => e
-            Rails.logger.debug "  [WALLABY] Preload warning: #{e.message} from #{file_path}"
-            Rails.logger.debug e.backtrace.slice(0, 5)
-          end
-        end
+        Dir.glob("#{load_path}/**/*.rb").sort.each(&method(:load_class_for))
       end
 
       protected
@@ -33,6 +21,19 @@ module Wallaby
         Rails.configuration.eager_load_paths.sort_by do |path|
           - path.index(%r{/models$}).to_i
         end
+      end
+
+      # `constantize` is used to make Rails to handle all sort of load errors
+      # NOTE: don't try to use `ActiveSupport::Dependencies::Loadable.require_dependency`
+      # as `require_dependency` does not take care all errors when class/module is declared
+      # @param file_path [Pathname, String]
+      def load_class_for(file_path)
+        module_name = file_path[%r{app/[^/]+/(.+)\.rb}, 1].gsub('/concerns/', '/')
+        class_name = module_name.camelize
+        class_name.constantize unless Module.const_defined? class_name
+      rescue NameError, LoadError => e
+        Rails.logger.debug "  [WALLABY] Preload warning: #{e.message} from #{file_path}"
+        Rails.logger.debug e.backtrace.slice(0, 5)
       end
     end
   end
