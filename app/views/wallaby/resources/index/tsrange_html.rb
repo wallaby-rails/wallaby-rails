c3462fff231787ca60ca710e27cb313f60034e4e
Tian Chen <me@tian.im>
2019-03-03 23:13:53 +1100

    reduce daterange_html duplication

--- app/views/wallaby/resources/index/tsrange_html.rb
+++ app/views/wallaby/resources/index/tsrange_html.rb
@@ -1,17 +1,7 @@
 module Wallaby
   module Resources
     module Index
-      class TsrangeHtml < Cell
-        def render
-          if value.nil?
-            null
-          else
-            concat content_tag(:span, I18n.l(value.first, format: :short), class: 'from')
-            concat '...'
-            concat content_tag(:span, I18n.l(value.last, format: :short), class: 'to')
-            itooltip "#{I18n.l value.first} ... #{I18n.l value.last}", 'clock-o'
-          end
-        end
+      class TsrangeHtml < DaterangeHtml
       end
     end
   end
