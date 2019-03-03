64c9da2951afbb990868c34493c221fc5f1ae4ab
Tian Chen <me@tian.im>
2019-03-03 23:24:18 +1100

    reduce line duplication

--- app/views/wallaby/resources/index/lseg_html.rb
+++ app/views/wallaby/resources/index/lseg_html.rb
@@ -1,21 +1,7 @@
 module Wallaby
   module Resources
     module Index
-      class LsegHtml < Cell
-        def render
-          if value.nil?
-            null
-          else
-            max = metadata[:max] || default_metadata.max
-            self.value = value.to_s
-            if value.length > max
-              concat content_tag(:code, value.truncate(max))
-              itooltip value
-            else
-              content_tag :code, value
-            end
-          end
-        end
+      class LsegHtml < LineHtml
       end
     end
   end
