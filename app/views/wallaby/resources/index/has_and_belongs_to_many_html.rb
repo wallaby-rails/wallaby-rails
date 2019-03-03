518d7b6212cab7b38daf473051dc1709cce4512f
Tian Chen <me@tian.im>
2019-03-03 23:11:23 +1100

    reduce many html duplication

--- app/views/wallaby/resources/index/has_and_belongs_to_many_html.rb
+++ app/views/wallaby/resources/index/has_and_belongs_to_many_html.rb
@@ -1,20 +1,7 @@
 module Wallaby
   module Resources
     module Index
-      class HasAndBelongsToManyHtml < Cell
-        def render
-          unique_collection = value.uniq
-          concat raw unique_collection.first(2).map { |item| show_link item, options: { readonly: true } }.join(', ')
-          if unique_collection.length > 2
-            title = metadata[:label]
-            body = unique_collection.map { |item| show_link item, options: { readonly: true } }.to_sentence.html_safe
-            label = "#{unique_collection.length - 2} more"
-            concat 'and'
-            imodal title, body, label: label
-          elsif unique_collection.blank?
-            null
-          end
-        end
+      class HasAndBelongsToManyHtml < HasManyHtml
       end
     end
   end
