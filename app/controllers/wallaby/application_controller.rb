1ee42dd347400b14841929b1c102474ab4028e75
Tian Chen <me@tian.im>
2019-03-06 22:43:28 +1100

    move error template to wallaby/resources folder

--- app/controllers/wallaby/application_controller.rb
+++ app/controllers/wallaby/application_controller.rb
@@ -67,7 +67,7 @@ module Wallaby
       @exception = exception
       @symbol = symbol
       @code = Rack::Utils::SYMBOL_TO_STATUS_CODE[symbol].to_i
-      respond_with @exception, status: @code, template: ERROR_PATH
+      respond_with @exception, status: @code, template: ERROR_PATH, prefixes: _prefixes
     end
   end
 end
