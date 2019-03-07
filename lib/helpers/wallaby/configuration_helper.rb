b12788b45458533c515583156bc2b8284754302e
Tian Chen <me@tian.im>
2019-03-07 12:12:39 +1100

    make rubocop happier than before

--- lib/helpers/wallaby/configuration_helper.rb
+++ lib/helpers/wallaby/configuration_helper.rb
@@ -1,12 +1,14 @@
 module Wallaby
   # Configuration helper module. Provide shortcut methods to configurations.
   module ConfigurationHelper
-    delegate(*%i(models security mapping pagination features sorting), to: :configuration)
+    delegate :models, :security, :mapping, :pagination, :features, :sorting, to: :configuration
+
     # @return [Wallaby::Configuration] shortcut method of configuration
     def configuration
       Wallaby.configuration
     end
 
+    # @return [Wallaby::Configuration::Metadata] shortcut method of metadata
     def default_metadata
       configuration.metadata
     end
