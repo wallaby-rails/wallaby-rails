98648e15c7f640cabc375bad1451d2fcc663a006
Tian Chen <me@tian.im>
2019-04-07 08:52:35 +1000

    remove abstract_responder

--- lib/responders/wallaby/resources_responder.rb
+++ lib/responders/wallaby/resources_responder.rb
@@ -1,5 +1,24 @@
 module Wallaby
   # Wallaby resource responder
-  class ResourcesResponder < AbstractResponder
+  class ResourcesResponder < ActionController::Responder
+    include ::Responders::FlashResponder
+
+    # @return [String] CSV
+    def to_csv
+      controller.headers['Content-Disposition'] = "attachment; filename=\"#{file_name_to_export}\""
+      default_render
+    end
+
+    protected
+
+    # @return [String] file name
+    def file_name_to_export
+      timestamp = Time.zone.now.to_s(:number)
+      "#{request.params[:resources]}-exported-#{timestamp}.#{format}"
+    end
+
+    def has_errors? # rubocop:disable Naming/PredicateName
+      resource.nil? || resource.is_a?(Exception) || controller.decorate(resource).errors.present?
+    end
   end
 end
