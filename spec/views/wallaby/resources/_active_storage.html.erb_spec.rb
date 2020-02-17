5e82b43ba54421f8fe8499871a08a522300f2c1b
Tian Chen <me@tian.im>
2020-02-18 01:04:39 +1100

    chore: fix rubocop issue

--- spec/views/wallaby/resources/_active_storage.html.erb_spec.rb
+++ spec/views/wallaby/resources/_active_storage.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 field_name = 'binary'
 describe field_name do
   it_behaves_like \
-    "index partial", field_name,
+    'index partial', field_name,
     action: '',
     partial_name: 'active_storage',
     value: true,
